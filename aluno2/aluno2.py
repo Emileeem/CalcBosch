e5330b1b08ab9b8500e9d98d5832f17665e5c6d8
Maycon <mayconbrunob@hotmail.com>
2023-03-21 08:59:20 -0300

    Adicionando Divisão

--- aluno2/aluno2.py
+++ aluno2/aluno2.py
@@ -2,3 +2,8 @@ def multiplicacao():
     v1 = float(input("Digite o primeiro valor da operação: "))
     v2 = float(input("Digite o segundo valor da operação: "))
     return v1*v2
+
+def divisao():
+    v1 = float(input("Digite o primeiro valor da operação: "))
+    v2 = float(input("Digite o segundo valor da operação: "))
+    return v1/v2
