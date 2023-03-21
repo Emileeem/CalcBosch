b5f273308e27f9811774a5536fc6e28affb15ffe
anabelly <anabellymontibeller@gmail.com>
2023-03-21 08:56:27 -0300

    adicionando subtracao

--- Aluno 1/aluno1.py
+++ Aluno 1/aluno1.py
@@ -2,3 +2,8 @@ def soma ():
     v1 = float(input("Digite o primeiro valor da operação: "))
     v2 = float(input("Digite o segundo valor da operação: "))
     print (v1 + v2)
+
+def subtracao():
+    v1 = float(input("Digite o primeiro valor da operação: "))
+    v2 = float(input("Digite o segundo valor da operação"))
+    print (v1 - v2)
